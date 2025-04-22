8b9a3c338f5b83f28b891ea157f6fff0faa77b20
Emil Galiyev <emil333.mem@gmail.com>
2025-04-22 17:25:08 +0500

    feat:direction enum class

--- src/main/java/STORMSPRID/model/Direction.java
+++ src/main/java/STORMSPRID/model/Direction.java
@@ -1,4 +1,26 @@
 package STORMSPRID.model;
 
 public enum Direction {
+    UP(-1,0),
+    RIGHT(0,1),
+    DOWN(1,0),
+    LEFT(0,-1);
+
+    private final int rowChange;
+    private final int colChange;
+
+    Direction(int rowChange, int colChange) {
+        this.rowChange = rowChange;
+        this.colChange = colChange;
+    }
+
+    public static Direction of(int rowChange,int colChange){
+        for(Direction direction:values()){
+            if (direction.rowChange ==rowChange
+            && direction.colChange ==colChange) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }
